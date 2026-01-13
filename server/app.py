77afab22d9017b2aacf289a71bcab03c615c329a
Abdimalik <binabdimalikranger@gmail.com>
2026-01-13 21:43:42 +0300

    fix

--- server/app.py
+++ server/app.py
@@ -1,8 +1,35 @@
-#!/usr/bin/env python3
-
 from flask import Flask
 
 app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "<h1>Python Operations with Flask Routing and Views</h1>"
+
+@app.route('/print/<parameter>')
+def print_string(parameter):
+    print(parameter)
+    return str(parameter)
+
+@app.route('/count/<int:parameter>')
+def count(parameter):
+    result = ""
+    for i in range(parameter):
+        result += f"{i}\n"
+    return result
+
+@app.route('/math/<int:num1>/<operation>/<int:num2>')
+def math(num1, operation, num2):
+    if operation == '+':
+        return str(num1 + num2)
+    elif operation == '-':
+        return str(num1 - num2)
+    elif operation == '*':
+        return str(num1 * num2)
+    elif operation == 'div':
+        return str(num1 / num2)
+    elif operation == '%':
+        return str(num1 % num2)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
